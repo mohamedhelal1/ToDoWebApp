473f647fd90bdc7cce436845daba24894d9b87b6
mohamedHelal <m.h.helal1997@gmail.com>
2018-11-21 06:17:57 +0200

    created test cases for todo

---
+++ test/unit/auth.spec.js
@@ -0,0 +1,83 @@
+require("dotenv").config();
+const chakram = require("chakram");
+const expect = chakram.expect;
+var faker = require('faker');
+var token;
+var email;
+var firstname;
+var lastname;
+
+/// because I did not deploy a db, I did not find the need to test in another db so testing is done in the same db
+describe("Auth", () => {
+    before(async () => {
+        email=faker.internet.email();///fakes an email
+        firstname= faker.name.firstName();// fakes firstname
+        lastname = faker.name.lastName();// fakes flast name
+        token = await chakram.post("http://localhost:3000/api"+ "/auth/signup", {//sends request
+            email: email,
+            password: "Pp123456",
+            firstname: firstname,
+            lastname: lastname,
+        });
+        expect(token).to.have.status(200);//checks if response status 200
+        token = token.body.token;
+    });
+    /// sign up
+    it("should sign up a parent", async () => {
+        var res = await chakram.post("http://localhost:3000/api" + "/auth/signup", {//sends request
+            email: faker.internet.email(),
+            password: "Pp123456",
+            firstname: faker.name.firstName(),
+            lastname: faker.name.lastName(),
+        });
+        expect(token).to.have.status(200);
+        expect(res.body.token).to.exist;//check if token exists 
+        expect(res.body.msg).to.be.equal("signed up successfully");
+    }); 
+   
+    it("should fail to sign up because of invalid password", async () => {
+        var res = await chakram.post("http://localhost:3000/api" + "/auth/signup", {//sends request
+            email: faker.internet.email(),
+            password: "P1256",
+            firstname: faker.name.firstName(),
+            lastname: faker.name.lastName(),
+        });
+        expect(res).to.have.status(422);// checks on status code for invalid
+        expect(res.body.err).to.be.equal("Password must be at least 8 letters long, contain a capital letter, contain a number.");
+    }); 
+    it("should fail to sign up same email twice", async () => {
+        var res = await chakram.post("http://localhost:3000/api"+ "/auth/signup", {
+            email: email,
+            password: "Pp123456",
+            firstname: faker.name.firstName(),
+            lastname: faker.name.lastName(),
+        });
+        expect(res).to.have.status(401);// checks on status code for unauthorized
+        expect(res.body.err).to.be.equal("this email already exist");
+    }); 
+    it("should login a user", async () => {
+        var res = await chakram.post("http://localhost:3000/api"+ "/auth/login", {
+            email: email,
+            password: "Pp123456",
+        });
+        expect(res).to.have.status(200);// checks on status code for ok
+        expect(res.body.token).to.exist;// checks if token exist
+        expect(res.body.msg).to.be.equal("logged in successfully");
+    }); 
+    it("should fail to login an unknown email", async () => {
+        var res = await chakram.post("http://localhost:3000/api" + "/auth/login", {
+            email: "ggg.gmail.com",
+            password: "Pp123456",
+        });
+        expect(res).to.have.status(404);// checks on status code for notfound
+        expect(res.body.err).to.be.equal("email is not found");
+    }); 
+    it("should fail to login with wrong password", async () => {
+        var res = await chakram.post("http://localhost:3000/api" + "/auth/login", {
+            email: email,
+            password: "Pp12345678",
+        });
+        expect(res).to.have.status(401);//check on status code for unauthorized
+        expect(res.body.err).to.be.equal("wrong password");
+    });
+});
---
+++ test/unit/todo.spec.js
@@ -0,0 +1,176 @@
+require("dotenv").config();
+const chakram = require("chakram");
+const expect = chakram.expect;
+var faker = require('faker');
+var token;
+var id;
+var id2;
+
+/// because I did not deploy a db, I did not find the need to test in another db so testing is done in the same db
+describe("Todo", () => {
+    before(async () => {
+        token = await chakram.post("http://localhost:3000/api"+ "/auth/signup", {//sends request
+            email: faker.internet.email(),
+            password: "Pp123456",
+            firstname: faker.name.firstName(),
+            lastname: faker.name.lastName(),
+        });
+        expect(token).to.have.status(200);//checks if response status 200
+        token = token.body.token;
+        var res = await chakram.post("http://localhost:3000/api"+ "/todo", {//sends request
+            headers:{'Authorization':token},
+            subject: faker.lorem.sentence(),
+            comment: faker.lorem.sentences(),
+            time: faker.date.recent(2),
+        });
+        expect(res).to.have.status(200);
+        id=res.body.id;
+        res = await chakram.post("http://localhost:3000/api"+ "/todo", {//sends request
+            headers:{'Authorization':token},
+            subject: faker.lorem.sentence(),
+            comment: faker.lorem.sentences(),
+            time: faker.date.recent(2),
+        });
+        expect(res).to.have.status(200);
+        id2=res.body.id;
+    });
+    /// create a todo
+    it("should create a todo", async () => {
+        var res = await chakram.post("http://localhost:3000/api"+ "/todo", {//sends request
+            headers:{'Authorization':token},
+            subject: faker.lorem.sentence(),
+            comment: faker.lorem.sentences(),
+            time: faker.date.recent(2),
+        });
+        expect(res).to.have.status(200);
+        expect(res.body.subject).to.exist;//check if res params exists 
+        expect(res.body.comment).to.exist;
+        expect(res.body.id).to.exist;
+        expect(res.body.userId).to.exist;
+        expect(res.body.msg).to.be.equal("added todo successfully");
+    }); 
+   
+    it("should fail to create a todo because user is not logged in", async () => {
+        var res = await chakram.post("http://localhost:3000/api"+ "/todo", {//sends request
+            subject: faker.lorem.sentence(),
+            comment: faker.lorem.sentences(),
+            time: faker.date.recent(2),
+        });
+        expect(res).to.have.status(401);
+        expect(res.body.err).to.be.equal("unauthorized");
+    });  
+    // get
+    it("should get a todo", async () => {
+        var res = await chakram.get("http://localhost:3000/api"+ "/todo/"+id, {//sends request
+            headers:{'Authorization':token},
+    
+        });
+        expect(res).to.have.status(200);
+        expect(res.body.subject).to.exist;//check if res params exists 
+        expect(res.body.comment).to.exist;
+        expect(res.body.id).to.exist;
+        expect(res.body.userId).to.exist;
+        expect(res.body.msg).to.be.equal("returned todo successfully");
+    }); 
+   
+    it("should fail to get a todo because user is not logged in", async () => {
+
+        var res = await chakram.get("http://localhost:3000/api"+ "/todo/"+id, {//sends request
+    
+        });
+        expect(res).to.have.status(401);
+        expect(res.body.err).to.be.equal("unauthorized");
+    });  
+
+    it("should fail to get a todo because item does not exist", async () => {
+
+        var res = await chakram.get("http://localhost:3000/api"+ "/todo/24234", {//sends request
+        headers:{'Authorization':token},
+        });
+        expect(res).to.have.status(404);
+        expect(res.body.err).to.be.equal("not found");
+    });  
+
+   // update 
+    it("should update a todo", async () => {
+        var res = await chakram.put("http://localhost:3000/api"+ "/todo/"+id, {//sends request
+            headers:{'Authorization':token},
+            subject: faker.lorem.sentence(),
+            comment: faker.lorem.sentences(),
+            time: faker.date.recent(2),
+        });
+        expect(res).to.have.status(200);
+        expect(res.body.subject).to.exist;//check if res params exists 
+        expect(res.body.comment).to.exist;
+        expect(res.body.id).to.exist;
+        expect(res.body.userId).to.exist;
+        expect(res.body.msg).to.be.equal("updated todo successfully");
+    }); 
+   
+    it("should fail to update a todo because user is not logged in", async () => {
+
+        var res = await chakram.put("http://localhost:3000/api"+ "/todo/"+id, {//sends request
+            subject: faker.lorem.sentence(),
+            comment: faker.lorem.sentences(),
+            time: faker.date.recent(2),
+        });
+        expect(res).to.have.status(401);
+        expect(res.body.err).to.be.equal("unauthorized");
+    });  
+
+    it("should fail to update a todo because item does not exist", async () => {
+
+        var res = await chakram.put("http://localhost:3000/api"+ "/todo/24234", {//sends request
+            headers:{'Authorization':token},
+            subject: faker.lorem.sentence(),
+            comment: faker.lorem.sentences(),
+            time: faker.date.recent(2),
+        });
+        expect(res).to.have.status(404);
+        expect(res.body.err).to.be.equal("not found");
+    }); 
+    
+    
+    it("should delete a todo", async () => {
+        var res = await chakram.delete("http://localhost:3000/api"+ "/todo/"+id2, {//sends request
+            headers:{'Authorization':token},
+        });
+        expect(res).to.have.status(200);
+        expect(res.body.subject).to.exist;//check if res params exists 
+        expect(res.body.comment).to.exist;
+        expect(res.body.id).to.exist;
+        expect(res.body.userId).to.exist;
+        expect(res.body.msg).to.be.equal("deleted todo successfully");
+    }); 
+   
+    it("should fail to delete a todo because user is not logged in", async () => {
+
+        var res = await chakram.delete("http://localhost:3000/api"+ "/todo/"+id, {//sends request
+    
+        });
+        expect(res).to.have.status(401);
+        expect(res.body.err).to.be.equal("unauthorized");
+    });  
+
+    it("should fail to delete a todo because item does not exist", async () => {
+
+        var res = await chakram.delete("http://localhost:3000/api"+ "/todo/24234", {//sends request
+        headers:{'Authorization':token},
+        });
+        expect(res).to.have.status(404);
+        expect(res.body.err).to.be.equal("not found");
+    }); 
+    it("should get user todos with a limit", async () => {
+        var res = await chakram.get("http://localhost:3000/api"+ "/todo/0/5", {//sends request
+            headers:{'Authorization':token},
+        });
+        expect(res).to.have.status(200);
+        expect(res.body.msg).to.be.equal("returned todos successfully");
+    }); 
+    it("should fail get user todos because user is not logged in", async () => {
+        var res = await chakram.get("http://localhost:3000/api"+ "/todo/0/5", {//sends request
+        });
+        expect(res).to.have.status(401);
+        expect(res.body.msg).to.be.equal("unauthorized");
+    }); 
+});
